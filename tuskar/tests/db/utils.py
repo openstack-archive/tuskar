ece5d22309c2465c657c8bbe688802d5c0852653
marios <marios@redhat.com>
2013-08-14 16:19:36 +0300

    Initial import of database tests - exercises the db_api, more to come.
    Change-Id: I41c6ad35b1491938f253374e22807220fecdfcb4

---
+++ tuskar/tests/db/utils.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2013 Red Hat
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tuskar.api.controllers.v1.types.resource_class import ResourceClass
+from tuskar.api.controllers.v1.types.flavor import Flavor
+from tuskar.api.controllers.v1.types.capacity import Capacity
+from tuskar.api.controllers.v1.types.rack import Rack
+from tuskar.api.controllers.v1.types.relation import Relation
+from tuskar.api.controllers.v1.types.link import Link
+from tuskar.api.controllers.v1.types.node import Node
+
+def get_test_resource_class(**kwargs):
+  rc = ResourceClass(name = kwargs.get('name', 'test_resource_class'),
+                     service_type = kwargs.get('type', 'compute'))
+  return rc
+
+def get_test_flavor(**kwargs):
+  flavor = Flavor(name = kwargs.get('name', 'one'),
+                     capacities = [
+                       Capacity(name = 'cpu',
+                                value=  kwargs.get('value', '1'),
+                                unit = 'count' ),
+                       Capacity(name = 'memory',
+                                value=  kwargs.get('value', '1'),
+                                unit = 'MiB' ),
+                       Capacity(name = 'storage',
+                                value=  kwargs.get('value', '1'),
+                                unit = 'GiB' )])
+  return flavor
+
+def get_test_rack(**kwargs):
+  rack = Rack(subnet = kwargs.get('subnet', '192.168.1.0/255'),
+              slots  = kwargs.get('slots', 1),
+              name   = kwargs.get('name', 'my_rack'),
+              capacities = [
+                 Capacity(name = 'total_cpu',
+                          value=  kwargs.get('total_cpu', '64'),
+                          unit = 'count' ),
+                 Capacity(name = 'total_memory',
+                          value=  kwargs.get('total_memory', '8192'),
+                          unit = 'MiB' )],
+              nodes  = [
+                 Node(id = '123'),
+                 Node(id = '345')])
+  return rack
+
+def get_test_resource_class_rack(**kwargs):
+  rack_id = kwargs.get('id', 1)
+  rc_rack = Relation( id = rack_id,
+                   links = [Link(href = 'http://0.0.0.0:6385/v1/racks/' +
+                            str(rack_id), rel  = 'self')])
+  return rc_rack
