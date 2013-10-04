ac73de271f2105c0b9b1fefbef82344bd83356cf
marios <marios@redhat.com>
2013-10-04 15:46:10 +0300

    blueprint retrieve-ooo-templates
    During startup of tuskar server, make sure we have tripleo-heat-templates.
    
    If we don't they are pulled in from github, paths created as necessary. Two
    conf vars are added: CONF.tht_local_dir for the local path in which the templates
    exist, and CONF.tht_git_repo for the git repo from which to clone templates.
    I have left the 'freeze at particular commit' code commented out - can be used
    for demo purposes if necessary.
    
    Implements: blueprint retrieve-ooo-templates
    https://blueprints.launchpad.net/tuskar/+spec/retrieve-ooo-templates
    
    Change-Id: I7cb1151f35aef00f416a7d06be6229553fbe9d22

--- tuskar/cmd/api.py
+++ tuskar/cmd/api.py
@@ -24,6 +24,7 @@ import logging
 import sys
 
 from oslo.config import cfg
+import os
 from wsgiref import simple_server
 
 from tuskar.api import app
@@ -32,6 +33,14 @@ from tuskar.openstack.common import log
 
 CONF = cfg.CONF
 
+def fetch_remote_heat_templates(template_path):
+    template_git = CONF.tht_git_repo
+    os.system("cd "+template_path+" ; git clone "+template_git)
+    #HAX: revert to a specific known-working commit for HK/freeze:
+    #uncomment next 3 lines to use:
+    # commit_id="0326335160a5977df44ae40d4c81ab8e40833743"
+    # os.system("cd "+template_path+"/tripleo-heat-templates; git "+
+    #           "reset --hard "+ commit_id )
 
 def main():
     # Pase config file and command line options, then start logging
@@ -48,6 +57,22 @@ def main():
     LOG.info("Serving on http://%s:%s" % (host, port))
     LOG.info("Configuration:")
     CONF.log_opt_values(LOG, logging.INFO)
+
+    # make sure we have tripleo-heat-templates:
+    heat_template_path = CONF.tht_local_dir +"tripleo-heat-templates"
+    try:
+        templates = os.listdir(heat_template_path)
+        if len(templates) < 1:
+            LOG.info("Can't find your local tripleo-heat-templates at "+
+                     heat_template_path+". Pulling from git repo at " +
+                     CONF.tht_git_repo)
+            fetch_remote_heat_templates(CONF.tht_local_dir)
+    except OSError:
+            LOG.info("Can't find your local tripleo-heat-template directory "+
+                     "at "+CONF.tht_local_dir + ". Pulling from git repo "+
+                     "at "+CONF.tht_git_repo)
+            fetch_remote_heat_templates(CONF.tht_local_dir)
+    LOG.info("Working with tripleo-heat-templates at"+heat_template_path)
 
     try:
         wsgi.serve_forever()
