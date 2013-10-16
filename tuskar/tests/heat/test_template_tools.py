78d2a90c46d2048ba45922d659e3e1054e3aabbd
marios <marios@redhat.com>
2013-10-16 12:31:39 +0300

    Using merge.py from tuskar to manage the stack
    Generate the yaml file describing Tuskar compute nodes and
    invoke merge.py, now that it is available. This review
    adds tripleo-heat-templates as requirement from master
    @github
    
    Now assuming hardcoded parameters, so we have at least something
    working. Connecting to real parameters will go as follow up patch.
    
    now create spawns 1 compute, update 2 compute nodes
    
    TODO: add kickass bulletproof tests in follow up patches
    TODO: fix all FIXMEs in follow-up patches
    
    TO TEST - routes defined for debug/test:
    
    Create the overcloud:
    curl -H "Content-Type:application/json"\
     -XPOST http://localhost:8585/v1/overclouds/ -d '{}'
    
    Update the overcloud:
    curl -H "Content-Type:application/json"\
     -XPUT http://localhost:8585/v1/overclouds/1 -d '{}'
    
    Delete the overcloud:
    curl -H "Content-Type:application/json"\
     -XDELETE http://localhost:8585/v1/overclouds/1 -d '{}'
    
    Change-Id: I578b4e9f238590ea245b827bc75d252568d194fe

---
+++ tuskar/tests/heat/test_template_tools.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+from tuskar.heat import template_tools
+from tuskar.tests import base
+
+
+URL_OVERCLOUDS = '/v1/overclouds'
+
+
+class TemplateToolsTests(base.TestCase):
+    def setUp(self):
+        super(TemplateToolsTests, self).setUp()
+
+    @mock.patch('tripleo_heat_merge.merge.parse_scaling',
+                return_value={'NovaCompute0': 12})
+    def test_generate_scaling_params(self, mock_scaling):
+        # Setup
+        rcs = {'overcloud_roles': {'controller': 1, 'compute': 12},
+               'fake': 'fun'}
+
+        # Test
+        result = template_tools.generate_scaling_params(rcs)
+
+        # Verify
+        self.assertEqual(result, {'NovaCompute0': 12})
+
+    @mock.patch('tripleo_heat_merge.merge.parse_scaling',
+                return_value={'NovaCompute0': 12})
+    @mock.patch('tripleo_heat_merge.merge.merge',
+                return_value='template')
+    def test_merge_templates(self, mock_merge, mock_scaling):
+        # Setup
+        rcs = {'overcloud_roles': {'controller': 1, 'compute': 12},
+               'fake': 'fun'}
+
+        # Test
+        result = template_tools.merge_templates(rcs)
+
+        # Verify
+        self.assertEqual(result, 'template')
