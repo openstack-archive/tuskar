37f1324e8bb8f8ac6bb0b3e5922b9525193461c4
marios <marios@redhat.com>
2013-10-16 12:31:39 +0300

    WIP: Using merge.py from tuskar to generate overcloud.yaml
    Generate the yaml file describing Tuskar compute nodes and
    invoke merge.py, now that it is available. This review
    adds tripleo-heat-templates as requirement from master
    @github... not sure what the best way is for making the
    tripleo-heat-merge module available here... hints?
    
    Based on latest design and wireframes UI will no longer be
    managing ResourceClasses and Racks through Tuskar API
    requests. For now assuming Tuskar will be getting a dict
    with 'type':count - e.g. {'compute':2, 'controller':1, ...
    
    TO TEST:
    since we still need controllers for the new UI models
    and discussion around the api, I attach the template code
    to the debug 'GET /data_centers/template' route:
    
    curl http://localhost:8585/v1/data_centers/template
    
    The code here uses a hard-coded structure which simulates
    what the UI will eventually send in the 'deploy' request
    (4 compute, 1 object, 2 block, 1 controller). The output
    for this particular example is at
    
    http://mariosandreou.com/documents/example_template.txt
    
    I would be very grateful for any sanity checks on that
    template given the example above.
    
    blueprint tripleo-heat-elements
    
    Change-Id: I578b4e9f238590ea245b827bc75d252568d194fe

--- tuskar/api/controllers/v1/data_center.py
+++ tuskar/api/controllers/v1/data_center.py
@@ -1,14 +1,14 @@
 #from oslo.config import cfg
 import pecan
-from pecan.core import render
+#from pecan.core import render
 from pecan import rest
 import wsme
 #from wsme import api
 #from wsme import types as wtypes
 #import wsmeext.pecan as wsme_pecan
 
-from tuskar.compute.nova import NovaClient
 from tuskar.heat.client import HeatClient as heat_client
+import tuskar.heat.template_tools as template_tools
 
 #from tuskar.common import exception
 #from tuskar.openstack.common import log
@@ -25,46 +25,65 @@ class DataCenterController(rest.RestController):
         heat = heat_client()
         return heat.get_stack().to_dict()
 
+    TEMP_RCS = {'resource_categories': {'controller': 1, 'compute': 2, 'block': 1}}
+
     @pecan.expose()
     def template(self):
-        rcs = pecan.request.dbapi.get_heat_data()
-        nova_utils = NovaClient()
-        return render('overcloud.yaml', dict(resource_classes=rcs,
-                                             nova_util=nova_utils))
+        #TODO(marios): what are we getting, exactly from the UI?
+        #for now assuming:
+        #
+        #Look at review: /#/c/66062/. No more ResourceClasses...expecting
+        #something like:
+        # {
+        #     'resource_categories': { 'controller': 1, 'compute': 4,
+        #                                'object': 1, 'block': 2}
+        # }
+        #NO LONGER DOING THIS:
+        #rcs = pecan.request.dbapi.get_heat_data()
+        rcs = self.TEMP_RCS
+        #keep it super simple at first       'object': 1, 'block': 2}}
+        #categories = pecan.request.dbapi.get_resource_categories()
+        categories = []
+        tuskar_template = template_tools.generate_template(rcs, categories)
+        overcloud = template_tools.merge_templates(tuskar_template)
+        return overcloud
+
+    def __debug_method(self, heat_client, template_txt):
+        heat_client.validate_template(template_txt)
+        template_file = open("/home/m/Downloads/OUT.txt", "r")
+        template_txt = template_file.read()
+        heat_client.validate_template(template_txt)
 
     @pecan.expose('json')
     def post(self):
-        # TODO(): Currently all Heat parameters are hardcoded in
-        #       template.
-        params = {}
-        rcs = pecan.request.dbapi.get_heat_data()
+        #TODO(marios): what are we getting, exactly from the UI?
+        #for now assuming:
+        #
+        #Look at review: /#/c/66062/. No more ResourceClasses...expecting
+        #something like:
+        # {
+        #     'resource_categories': { 'controller': 1, 'compute': 4,
+        #                                'object': 1, 'block': 2}
+        # }
+        #NO LONGER DOING THIS:
+        #rcs = pecan.request.dbapi.get_heat_data()
+        rcs = self.TEMP_RCS
+        #keep it super simple at first       'object': 1, 'block': 2}}
+        #categories = pecan.request.dbapi.get_resource_categories()
+        categories = []
+        tuskar_template = template_tools.generate_template(rcs, categories)
+        overcloud = template_tools.merge_templates(tuskar_template)
         heat = heat_client()
-        nova_utils = NovaClient()
-
-        for resource in rcs:
-            service_type = resource.service_type
-            image_id = getattr(resource, "image_id", None)
-
-            if image_id:
-                if service_type == 'compute':
-                    params['NovaImage'] = image_id
-                elif service_type in ('not_compute', 'controller'):
-                    params['notcomputeImage'] = image_id
-
-        template_body = render('overcloud.yaml', dict(resource_classes=rcs,
-            nova_util=nova_utils))
-        if heat.validate_template(template_body):
-
+        #keeping this for now until heat client methods are reworked:
+        self.__debug_method(heat, overcloud)
+        params = {}
+        if heat.validate_template(overcloud):
             if heat.exists_stack():
-                res = heat.update_stack(template_body, params)
+                res = heat.update_stack(overcloud, params)
             else:
-                res = heat.create_stack(template_body, params)
+                res = heat.create_stack(overcloud, params)
 
             if res:
-                for rc in rcs:
-                    [pecan.request.dbapi.update_rack_state(
-                        r, 'CREATE_IN_PROGRESS') for r in rc.racks]
-
                 pecan.response.status_code = 202
                 return {}
             else:
