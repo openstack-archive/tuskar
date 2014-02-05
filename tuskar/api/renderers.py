f195a00f16da7aa2d8f7b015eba8c2173f64e238
Dougal Matthews <dougal@redhat.com>
2014-02-05 15:19:10 +0000

    Update the HTTP 500 JSON structure to match oslo
    Update the JSONRenderer to ouput fault JSON in the structure expected by
    clients based on the oslo common code. This change also adds test
    coverage for the renderer.
    
    Change-Id: I36f495cac687c3833a765ad91f9d834964fb27bd
    Closes-Bug: #1276465

--- tuskar/api/renderers.py
+++ tuskar/api/renderers.py
@@ -14,33 +14,58 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import json
-
 import pecan
 import wsme
 from wsme import api
 
 
-class JSonRenderer(object):
-    """Custom json renderer.
+class JSONRenderer(object):
+    """Custom JSON renderer.
 
-    Renders to json and handles responses for various HTTP status codes.
+    Renders to JSON and handles responses for various HTTP status codes.
     """
+
     def __init__(self, path, extra_vars):
-        pass
+        """Create an empty __init__ to accept the arguments provided to a
+        Renderer but ignore them as they are not needed.
+        """
+
+    def _render_fault(self, message, details, code=500):
+        """Given the namespace dictionary render a JSON error response for the
+        fault in the format defined by the OpenStack identity service
+        documentation.
+        """
+
+        body = {
+            'identityFault': {
+                "message": message,
+                "details": details,
+                "code": code
+            }
+        }
+        return wsme.rest.json.encode_error(None, body)
 
     def render(self, template_path, namespace):
+        """Given a namespace dict render the response as JSON and return.
+
+        If the dict contains a faultcode or wsme.api.Response its a fault from
+        user code and is rendered via _render_fault.
+
+        template_path is a required parameter for renderers but unused in
+        this context.
+        """
+
         if 'faultcode' in namespace:
-            return wsme.rest.json.encode_error(None, namespace)
+            return self._render_fault(namespace['faultstring'],
+                namespace['debuginfo'])
 
         result = namespace['result']
         if isinstance(namespace['result'], api.Response):
             pecan.response.status_code = result.status_code
-            val = json.dumps({'faultstring': result.obj.faultstring,
-                              'faultcode': result.obj.faultcode})
-            return val
+            return self._render_fault(result.obj.faultstring,
+                result.obj.debuginfo, code=result.status_code)
 
         return wsme.rest.json.encode_result(
-            namespace['result'],
+            result,
             namespace['datatype']
         )
