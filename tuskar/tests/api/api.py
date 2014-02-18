0400ec327817f46d501cab159744ac07a781532b
Petr Blaho <pblaho@redhat.com>
2014-02-18 15:10:08 +0100

    Removes old commented out code
    This removes old commented out code form tuskar/tests/api/api.py
    Removed code contained import and docstring, probably forgotten when
    refactoring before.
    
    Change-Id: I462ab815c045c053dc2435b6a55ecb6b01c161ce

--- tuskar/tests/api/api.py
+++ tuskar/tests/api/api.py
@@ -15,10 +15,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""Base classes for API tests.
-"""
-
-# import urllib
 
 
 from oslo.config import cfg
