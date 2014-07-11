cefdc2181ddae78cb5b476a1f30fd8844de24ebe
Petr Blaho <pblaho@redhat.com>
2014-07-11 16:41:10 +0200

    Adds GET /v2/plans endpoint to API
    This patch adds hollow GET /v2/plans endpoint which returns only dummy
    data.
    Logic to retrieve real data will be added later.
    
    Change-Id: Ieb13483cb22842607f442ccc16dc7fc2da618e4f
    Implements: blueprint tripleo-juno-tuskar-plan-rest-api

---
+++ tuskar/api/controllers/v2/controller.py
@@ -0,0 +1,36 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pecan
+
+from tuskar.api.controllers.v2.plans import PlansController
+
+
+class Controller(object):
+    """Version 2 API controller root."""
+
+    plans = PlansController()
+
+    @pecan.expose('json')
+    def index(self):
+        return {
+            'version': {
+                'status': 'development',
+                'media-types': [{'base': 'application/json'}],
+                'id': 'v2.0',
+                'links': [{
+                    'href': '/v2/',
+                    'rel': 'self',
+                }]
+            }
+        }
