750597a767b42e0527b9497facdfe004f5c4fbef
Dougal Matthews <dougal@redhat.com>
2014-08-06 17:09:36 +0100

    Removed unused mox testing utilities
    Change-Id: Ia81030f599f5712d020ba1fe83bc784785da2c71

--- tuskar/tests/test_utils.py
+++ tuskar/tests/test_utils.py
@@ -13,55 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-import os.path
-
-import mox
-import six.moves.builtins as __builtin__
-
 from tuskar.common import utils
 from tuskar.tests import base
-
-
-class GenericUtilsTestCase(base.TestCase):
-
-    def test_read_cached_file(self):
-        self.mox.StubOutWithMock(os.path, "getmtime")
-        os.path.getmtime(mox.IgnoreArg()).AndReturn(1)
-        self.mox.ReplayAll()
-
-        cache_data = {"data": 1123, "mtime": 1}
-        data = utils.read_cached_file("/this/is/a/fake", cache_data)
-        self.assertEqual(cache_data["data"], data)
-
-    def test_read_modified_cached_file(self):
-        self.mox.StubOutWithMock(os.path, "getmtime")
-        self.mox.StubOutWithMock(__builtin__, 'open')
-        os.path.getmtime(mox.IgnoreArg()).AndReturn(2)
-
-        fake_contents = "lorem ipsum"
-        fake_file = self.mox.CreateMockAnything()
-        fake_file.read().AndReturn(fake_contents)
-        fake_context_manager = self.mox.CreateMockAnything()
-        fake_context_manager.__enter__().AndReturn(fake_file)
-        fake_context_manager.__exit__(mox.IgnoreArg(),
-                                      mox.IgnoreArg(),
-                                      mox.IgnoreArg())
-
-        __builtin__.open(mox.IgnoreArg()).AndReturn(fake_context_manager)
-
-        self.mox.ReplayAll()
-        cache_data = {"data": 1123, "mtime": 1}
-        self.reload_called = False
-
-        def test_reload(reloaded_data):
-            self.assertEqual(reloaded_data, fake_contents)
-            self.reload_called = True
-
-        data = utils.read_cached_file(
-            "/this/is/a/fake", cache_data, reload_func=test_reload)
-        self.assertEqual(data, fake_contents)
-        self.assertTrue(self.reload_called)
 
 
 class IntLikeTestCase(base.TestCase):
