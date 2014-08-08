a030e68d6da968be03282b061ca730bedb0bd472
Dougal Matthews <dougal@redhat.com>
2014-08-08 15:16:59 +0100

    Remove unused openstack.common.rpc
    rpc was included in the original copy from Ironic and isn't
    used anymore.
    
    Change-Id: I0dbfa841baf108bf9f595d2eab0b610200193f13

--- tuskar/common/config.py
+++ tuskar/common/config.py
@@ -19,7 +19,6 @@ from oslo.config import cfg
 
 from tuskar.common import paths
 from tuskar.openstack.common.db.sqlalchemy import session as db_session
-from tuskar.openstack.common import rpc
 from tuskar import version
 
 _DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('$sqlite_db')
@@ -28,7 +27,6 @@ _DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('$sqlite_db')
 def parse_args(argv, default_config_files=None):
     db_session.set_defaults(sql_connection=_DEFAULT_SQL_CONNECTION,
                             sqlite_db='tuskar.sqlite')
-    rpc.set_defaults(control_exchange='tuskar')
     cfg.CONF(argv[1:],
              project='tuskar',
              version=version.version_info.release_string(),
