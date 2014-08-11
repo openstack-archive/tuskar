5f1df103695c0182d83ebd11134b6d05e2c80cca
Dougal Matthews <dougal@redhat.com>
2014-08-11 10:32:37 +0100

    Tuskar command to import a set of roles
    This change adds a command to import a set of roles into the Tuskar
    storage. This will typically be ran during the installation and setup to
    provide a set of initial roles.
    
    Change-Id: I3395363c709c89072025083ca3da3189aae9e5d9

---
+++ tuskar/cmd/load_roles.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import print_function
+
+import sys
+
+from oslo.config import cfg
+
+from tuskar.common import service
+from tuskar.storage.load_roles import load_roles
+
+
+def _print_names(message, names):
+    print("{0}: \n    {1}".format(message, '\n    '.join(names)))
+
+
+cfg.CONF.register_cli_opt(cfg.BoolOpt('dry-run'))
+cfg.CONF.register_cli_opt(cfg.StrOpt('directory', positional=True))
+
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv
+
+    service.prepare_service(argv)
+
+    all_roles, created, updated = load_roles(cfg.CONF.directory,
+                                             cfg.CONF.dry_run)
+
+    if len(created):
+        _print_names("Created", created)
+
+    if len(updated):
+        _print_names("Updated", updated)
+
+    if not cfg.CONF.dry_run:
+        print("Imported {0} roles".format(len(all_roles)))
+    else:
+        _print_names("Found", all_roles)
+        print("Imported 0 roles")
---
+++ tuskar/tests/cmd/test_load_roles.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from mock import call
+from mock import patch
+
+from tuskar.cmd import load_roles
+from tuskar.tests.base import TestCase
+
+
+class LoadRoleTests(TestCase):
+
+    @patch('tuskar.storage.load_roles._list_roles',
+           return_value=[['role_name.yaml', '/path/role_name.yaml']])
+    @patch('tuskar.storage.load_roles._read_role', return_value="YAML")
+    @patch('tuskar.cmd.load_roles._print_names')
+    def test_main(self, mock_print, mock_read, mock_list):
+
+        # test
+        load_roles.main("/path/".split())
+
+        # verify
+        self.assertEqual([
+            call('Created', ['role_name.yaml'])
+        ], mock_print.call_args_list)
