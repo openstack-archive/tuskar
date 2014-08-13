d1f685b1ae47e6d8c18b2a3d18b502677b103fad
Jay Dobies <jason.dobies@redhat.com>
2014-08-13 14:38:36 -0400

    Wires up REST API to underlying Tuskar logic
    This also includes updating the transfer objects with the necessary
    data.
    
    Change-Id: I81143d57425a22bb26821c5ad74270798c6dfd80
    Implements: blueprint tripleo-juno-tuskar-plan-rest-api

--- tuskar/api/controllers/v2/models.py
+++ tuskar/api/controllers/v2/models.py
@@ -17,9 +17,13 @@ module also contain the translations between the REST transfer objects and
 the internal Tuskar domain model.
 """
 
+import datetime
 import logging
 
 from wsme import types as wtypes
+
+from tuskar.manager import models as manager_models
+
 
 LOG = logging.getLogger(__name__)
 
@@ -44,13 +48,104 @@ class Base(wtypes.Base):
 class Role(Base):
     """Transfer object for roles."""
 
-    uuid = str
+    uuid = wtypes.text
     name = wtypes.text
+    version = int
+    description = wtypes.text
+
+    @classmethod
+    def from_tuskar_model(cls, role):
+        """Translates from the Tuskar domain model.
+
+        :type role: tuskar.manager.models.Role
+        """
+        r = cls(**{
+            'uuid': role.uuid,
+            'name': role.name,
+            'version': role.version,
+            'description': role.description
+        })
+        return r
+
+
+class PlanParameter(Base):
+
+    name = wtypes.text
+    label = wtypes.text
+    default = wtypes.text
+    description = wtypes.text
+    hidden = bool
+    value = wtypes.text
+
+    @classmethod
+    def from_tuskar_model(cls, param):
+        """Translates from the Tuskar domain model.
+
+        :type param: tuskar.manager.models.PlanParameter
+        """
+        p = cls(**{
+            'name': param.name,
+            'label': param.label,
+            'default': param.default,
+            'description': param.description,
+            'hidden': param.hidden,
+            'value': param.value,
+        })
+        return p
 
 
 class Plan(Base):
-    """Transfer object for plans."""
 
-    uuid = str
+    uuid = wtypes.text
     name = wtypes.text
-    roles = wtypes.ArrayType(Role)
+    description = wtypes.text
+    created_at = datetime.datetime
+    updated_at = datetime.datetime
+    roles = [Role]
+    parameters = [PlanParameter]
+
+    @classmethod
+    def from_tuskar_model(cls, plan):
+        """Translates from the Tuskar domain model.
+
+        :type plan: tuskar.manager.models.DeploymentPlan
+        """
+        roles = [Role.from_tuskar_model(r) for r in plan.roles]
+        params = [PlanParameter.from_tuskar_model(p) for p in plan.parameters]
+
+        p = cls(**{
+            'uuid': plan.uuid,
+            'name': plan.name,
+            'description': plan.description,
+            'created_at': plan.created_at,
+            'updated_at': plan.updated_at,
+            'roles': roles,
+            'parameters': params,
+        })
+        return p
+
+
+class ParameterValue(Base):
+
+    name = wtypes.text
+    value = wtypes.text
+
+    @classmethod
+    def from_tuskar_model(cls, param_value):
+        """Translates from the Tuskar domain model.
+
+        :type param_value: tuskar.manager.models.ParameterValue
+        """
+        p = cls(**{
+            'name': param_value.name,
+            'value': param_value.value,
+        })
+        return p
+
+    def to_tuskar_model(self):
+        """Translates into the Tuskar domain model.
+
+        :rtype: tuskar.manager.models.ParameterValue
+        """
+        p = manager_models.ParameterValue(self.name, self.value)
+        return p
