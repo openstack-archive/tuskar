fb1b190470a91c18d2316a7660474ce698d374d6
Jay Dobies <jason.dobies@redhat.com>
2014-09-16 10:48:09 -0400

    Hooked seed handling into the managers
    Added logic in the managers to load the master seed and, if one is
    present, perform the necessary steps to apply the seed defaults to the
    plan.
    
    Change-Id: I2548b501a082a2645ea9f3775b4cef158b3da07e

--- tuskar/templates/template_seed.py
+++ tuskar/templates/template_seed.py
@@ -32,6 +32,13 @@ plan's master template file using a seed template.
 """
 
 import copy
+import logging
+
+from tuskar.templates.heat import EnvironmentParameter
+from tuskar.templates.heat import Resource
+
+
+LOG = logging.getLogger(__name__)
 
 
 def add_top_level_resources(source, destination):
@@ -50,13 +57,14 @@ def add_top_level_resources(source, destination):
             destination.add_resource(cloned)
 
 
-def add_top_level_parameters(source, destination):
+def add_top_level_parameters(source, destination, environment):
     """Adds the top-level parameters from the source template into the given
     template. If the parameter is already in the destination template, it will
     not be added again.
 
     :type source: tuskar.templates.heat.Template
     :type destination: tuskar.templates.heat.Template
+    :type environment: tuskar.templates.heat.Environment
     """
 
     # Make a list of all property key names across all role resources
@@ -64,9 +72,19 @@ def add_top_level_parameters(source, destination):
 
     def _resource_property_keys(resource):
         keys = []
-        resource_def = resource.find_property_by_name('resource_def')
-        for name in resource_def.value['properties']:
-            keys.append(name)
+
+        for rp in resource.properties:
+            if rp.name in ('resource_def', 'resource'):
+                # For a resource definition, dig into the properties value
+                # within to get all of the inner resource properties.
+                for name in rp.value['properties']:
+                    keys.append(name)
+            else:
+                # For all other resource properties, if the value is a
+                # get_param lookup, consider the value of the look up a
+                # resource property.
+                if isinstance(rp.value, dict) and 'get_param' in rp.value:
+                    keys.append(rp.value['get_param'])
         return keys
 
     role_resources = [r for r in source.resources if _is_role(r)]
@@ -85,6 +103,9 @@ def add_top_level_parameters(source, destination):
         if destination.find_parameter_by_name(p.name) is None:
             cloned = copy.copy(p)
             destination.add_parameter(cloned)
+
+            ep = EnvironmentParameter(p.name, p.default or '')
+            environment.add_parameter(ep)
 
 
 def add_top_level_outputs(source, destination):
@@ -113,15 +134,17 @@ def get_property_map_for_role(source, role_name):
 
     :return: mapping of property name to top-level resource lookup (get_attr)
              for any complex property in the role; simple properties will not
-             be present in the dict
+             be present in the dict; None if the role is not found in the
+             source template
     :rtype:  dict
 
     :raises ValueError: if the role is not in the given template
     """
+    resource = _find_resource_by_case_insensitive_id(source.resources,
+                                                     role_name)
 
-    resource = source.find_resource_by_id(role_name)
     if resource is None:
-        raise ValueError('No resource found for role: %s' % role_name)
+        return None
 
     property_map = {}
     resource_def = resource.find_property_by_name('resource_def')
@@ -137,6 +160,49 @@ def get_property_map_for_role(source, role_name):
     return property_map
 
 
+def update_references(template, seed_role_name, tuskar_resource_name):
+    """Updates the global resources in the given template to change references
+    inside of the top-level resources from the seed's role name to the
+    name of the resource as added by Tuskar.
+
+    For example, the overcloud.yaml definition for the compute resource is
+    given the name "Compute", however when Tuskar generates the resource it
+    includes the role and version information and is named
+    "compute-1-servers".
+
+    Many of the top-level resources will want to reference the role in its
+    properties using one of two mechanisms: get_attr or get_resource. In
+    these cases, the lookup must be changed to the Tuskar-generated role name.
+
+    :type template: tuskar.templates.heat.Template
+    :type seed_role_name: str
+    :type tuskar_resource_name: str
+    """
+    top_level_resources = [r for r in template.resources if not _is_role(r)]
+
+    def index_property(update_me):
+        if isinstance(update_me, dict):
+            return update_me.items()
+        elif isinstance(update_me, list):
+            return enumerate(update_me)
+
+    def update_property(update_me):
+        for index, value in index_property(update_me):
+            if isinstance(value, (dict, list)):
+                update_property(value)
+            elif isinstance(value, basestring):
+                if value.lower() == seed_role_name.lower():
+                    update_me[index] = tuskar_resource_name
+            else:
+                LOG.warn('Unexpected type (%s) in property value (%s)' %
+                         (value.__class__.__name__, value))
+
+    for r in top_level_resources:
+        for p in r.properties:
+            if isinstance(p.value, dict):
+                update_property(p.value)
+
+
 def _is_role(resource):
     """Returns whether or not the given resource represents a role.
 
@@ -146,6 +212,36 @@ def _is_role(resource):
     scaling_groups = ('OS::Heat::ResourceGroup', 'OS::Heat::AutoScalingGroup')
     if resource.resource_type in scaling_groups:
         inner_resource = resource.find_property_by_name('resource_def')
-        return 'OS::TripleO::' in inner_resource.value['type']
+
+        if isinstance(inner_resource.value, Resource):
+            v = inner_resource.value.resource_type
+        else:
+            v = inner_resource.value['type']
+
+        return 'OS::TripleO::' in v
     else:
         return False
+
+
+def _find_resource_by_case_insensitive_id(resources, resource_id):
+    """Returns the resource or None if it is not found. While it is
+    technically possible that the template contains multiple resources with
+    the given ID in different cases, it's not likely. For safety, this method
+    will raise an error if that occurs.
+
+    :type resources: list of tuskar.templates.heat.Resource
+    :str resource_id: str
+    :rtype: tuskar.templates.heat.Resource
+    :raise ValueError: if the template contains more than one resource with
+                       the given ID (regardless of case)
+    """
+    resource_id = resource_id.lower()
+    matching = [r for r in resources if r.resource_id.lower() == resource_id]
+
+    if len(matching) > 1:
+        raise ValueError('Invalid template; contains multiple resources '
+                         'matching %s' % resource_id)
+    elif len(matching) == 1:
+        return matching[0]
+    else:
+        return None
