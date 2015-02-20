c06340637f31bdcb9229a56d3ff61700d6f9ca50
marios <marios@redhat.com>
2015-02-20 17:06:57 +0200

    Adds the /role/role_id/extra_data path to the v2 API
    This uses the new TemplateExtraStore to process stored extra-data
    files and match them against the template for each defined role
    
    This information is included when a client requests the existing
    /plans/plan_uuid/templates path. To retrieve the extra-data for
    any particular role a new /role/role_uuid/extra_data path is added
    
    Change-Id: Ic52280248b0d169f4a3c27e10a826d3dbb2b9bf3

--- tuskar/api/controllers/v2/roles.py
+++ tuskar/api/controllers/v2/roles.py
@@ -17,6 +17,7 @@ from wsmeext import pecan as wsme_pecan
 
 from tuskar.api.controllers.v2 import models
 from tuskar.common import exception
+from tuskar.common import utils
 from tuskar.manager.plan import PlansManager
 from tuskar.manager.role import RoleManager
 from tuskar.storage import exceptions as storage_exceptions
@@ -27,6 +28,8 @@ LOG = logging.getLogger(__name__)
 
 class RolesController(rest.RestController):
     """REST controller for the Role class."""
+
+    _custom_actions = {'extra_data': ['GET']}
 
     @wsme_pecan.wsexpose([models.Role])
     def get_all(self):
@@ -42,6 +45,46 @@ class RolesController(rest.RestController):
         all_roles = manager.list_roles(only_latest=False)
         transfer_roles = [models.Role.from_tuskar_model(r) for r in all_roles]
         return transfer_roles
+
+    @wsme_pecan.wsexpose({str: str}, str)
+    def extra_data(self, role_uuid):
+        """Retrieve the extra data files associated with a given role.
+
+        :param role_uuid: identifies the role
+        :type  role_uuid: str
+
+        :return: a dict where keys are filenames and values are their contents
+        :rtype:  dict
+
+        This method will retrieve all stored role_extra records (these are
+        created at the same time that the Roles are, by using --role-extra
+        parameter to tuskar-load-roles).
+
+        The internal representation for a given role-extra file encodes the
+        file extension into the name. For instance 'hieradata/compute.yaml'
+        is stored as 'extra_compute_yaml'.
+
+        The given role's template is searched for 'get_file' directives and
+        then matched against the stored role-extra records (based on their
+        name... e.g. 'extra_controller_yaml' we look for 'controller.yaml'
+        after a get_file directive).
+
+        This method thus returns all the matched role-extra files for the
+        given role. The keys will include the relative path if one is
+        used in the role template:
+        {
+            "hieradata/common.yaml": "CONTENTS",
+            "hieradata/controller.yaml": "CONTENTS",
+            "hieradata/object.yaml": "CONTENTS"
+        }
+
+        """
+        manager = RoleManager()
+        db_role = manager.retrieve_db_role_by_uuid(role_uuid)
+        db_role_extra = manager.retrieve_db_role_extra()
+        role_extra_paths = utils.resolve_template_extra_data(
+            db_role, db_role_extra)
+        return manager.template_extra_data_for_output(role_extra_paths)
 
     @wsme_pecan.wsexpose(models.Plan,
                          str,
