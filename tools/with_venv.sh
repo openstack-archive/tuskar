9b880c95ebcb7f1827e8e9f5e5903fb3f92f92cd
Dougal Matthews <dougal@redhat.com>
2016-01-21 15:10:12 +0000

    Retire the Tuskar codebase.
    Change-Id: If724a2cbde086d4c4341539a2e51ee4fd1915a26
    Depends-On: I904b2f27591333e104bf9080bb8c3876fcb3596c

--- tools/with_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tools_path=${tools_path:-$(dirname $0)}
-venv_path=${venv_path:-${tools_path}}
-tox_env=$(cd ${venv_path} && find ../.tox -maxdepth 1 -name "py*" | sort | tail -n1)
-venv_dir=${venv_name:-${tox_env}}
-TOOLS=${tools_path}
-VENV=${venv:-${venv_path}/${venv_dir}}
-source ${VENV}/bin/activate && "$@"
